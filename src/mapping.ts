b7b90ad8e23b2991cefac472650b051ad13e269a
sayedex <sayedadmod@gmail.com>
2022-04-28 12:45:28 +0600

    Initial commit

---
+++ src/mapping.ts
@@ -0,0 +1,147 @@
+import { BigInt } from "@graphprotocol/graph-ts"
+import {
+  TRDCvault,
+  Approval,
+  BuyWeapon,
+  GetCop,
+  GetThief,
+  GetWeapon,
+  GiveTreasure,
+  OwnershipTransferred,
+  RewardsClaimed,
+  Transfer,
+  WithdrawalBNB,
+  WithdrawalToken,
+  copHeist,
+  thiefHeist
+} from "../generated/TRDCvault/TRDCvault"
+import { ExampleEntity } from "../generated/schema"
+
+export function handleApproval(event: Approval): void {
+  // Entities can be loaded from the store using a string ID; this ID
+  // needs to be unique across all entities of the same type
+  let entity = ExampleEntity.load(event.transaction.from.toHex())
+
+  // Entities only exist after they have been saved to the store;
+  // `null` checks allow to create entities on demand
+  if (!entity) {
+    entity = new ExampleEntity(event.transaction.from.toHex())
+
+    // Entity fields can be set using simple assignments
+    entity.count = BigInt.fromI32(0)
+  }
+
+  // BigInt and BigDecimal math are supported
+  entity.count = entity.count + BigInt.fromI32(1)
+
+  // Entity fields can be set based on event parameters
+  entity.owner = event.params.owner
+  entity.spender = event.params.spender
+
+  // Entities can be written to the store with `.save()`
+  entity.save()
+
+  // Note: If a handler doesn't require existing field values, it is faster
+  // _not_ to load the entity from the store. Instead, create it fresh with
+  // `new Entity(...)`, set the fields that should be updated and save the
+  // entity back to the store. Fields that were not set or unset remain
+  // unchanged, allowing for partial updates to be applied.
+
+  // It is also possible to access smart contracts from mappings. For
+  // example, the contract that has emitted the event can be connected to
+  // with:
+  //
+  // let contract = Contract.bind(event.address)
+  //
+  // The following functions can then be called on this contract to access
+  // state variables and other data:
+  //
+  // - contract.PlayerID(...)
+  // - contract.TRDCplayer(...)
+  // - contract.allowance(...)
+  // - contract.approve(...)
+  // - contract.approveOnBothSides(...)
+  // - contract.bVA(...)
+  // - contract.balanceOf(...)
+  // - contract.bankVaultPowerReset(...)
+  // - contract.banks(...)
+  // - contract.busted(...)
+  // - contract.buyCard(...)
+  // - contract.buyWeapon(...)
+  // - contract.cardCop(...)
+  // - contract.cardNounce(...)
+  // - contract.cardPrice(...)
+  // - contract.cardThief(...)
+  // - contract.claimReward(...)
+  // - contract.copCardOwned(...)
+  // - contract.copsYes(...)
+  // - contract.copsYesC(...)
+  // - contract.currency(...)
+  // - contract.dEaD(...)
+  // - contract.decimals(...)
+  // - contract.decreaseAllowance(...)
+  // - contract.endTime(...)
+  // - contract.gameOperator(...)
+  // - contract.gamePublish(...)
+  // - contract.gameRunning(...)
+  // - contract.getBankVaults(...)
+  // - contract.getBigVault(...)
+  // - contract.getInVaults(...)
+  // - contract.giveCopCardV1(...)
+  // - contract.giveThiefCardV1(...)
+  // - contract.giveWeaponV1(...)
+  // - contract.half(...)
+  // - contract.increaseAllowance(...)
+  // - contract.isShielded(...)
+  // - contract.name(...)
+  // - contract.owner(...)
+  // - contract.percentageCut(...)
+  // - contract.player(...)
+  // - contract.playerCount(...)
+  // - contract.playerIsCop(...)
+  // - contract.playerIsHolder(...)
+  // - contract.playerStats(...)
+  // - contract.priceOfShield(...)
+  // - contract.returnPlayer(...)
+  // - contract.rewardPercentage(...)
+  // - contract.roundsCount(...)
+  // - contract.runCop(...)
+  // - contract.startHeistThief(...)
+  // - contract.symbol(...)
+  // - contract.thiefCardsOwned(...)
+  // - contract.thiefYesC(...)
+  // - contract.toStart(...)
+  // - contract.totalBurned(...)
+  // - contract.totalRewards(...)
+  // - contract.totalSupply(...)
+  // - contract.transfer(...)
+  // - contract.transferFrom(...)
+  // - contract.treasure(...)
+  // - contract.vVault(...)
+  // - contract.weapons(...)
+  // - contract.weaponsOwned(...)
+}
+
+export function handleBuyWeapon(event: BuyWeapon): void {}
+
+export function handleGetCop(event: GetCop): void {}
+
+export function handleGetThief(event: GetThief): void {}
+
+export function handleGetWeapon(event: GetWeapon): void {}
+
+export function handleGiveTreasure(event: GiveTreasure): void {}
+
+export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
+
+export function handleRewardsClaimed(event: RewardsClaimed): void {}
+
+export function handleTransfer(event: Transfer): void {}
+
+export function handleWithdrawalBNB(event: WithdrawalBNB): void {}
+
+export function handleWithdrawalToken(event: WithdrawalToken): void {}
+
+export function handlecopHeist(event: copHeist): void {}
+
+export function handlethiefHeist(event: thiefHeist): void {}
